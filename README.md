53e8d122e9ba30ee6a789f650fbf7becc18fb081
ricardotjr <ricardinhotjr@gmail.com>
2016-01-03 17:16:27 -0200

    fontes

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # xy_inc
+
+Para utilização dos fontes deverá ser instalado o postgresql e o tomcat na versão 7
+
+http://mirror.nbtelecom.com.br/apache/tomcat/tomcat-7/v7.0.67/bin/apache-tomcat-7.0.67.zip
+http://get.enterprisedb.com/postgresql/postgresql-9.5.0-rc1-windows-x64.exe
+
+A senha de banco do usuário postgres deverá ser alterada para 123456
+Deverá ser executado o script database.sql no postgres
+
+Compilar os fontes com a versão do java 7 e fazer o deploy do artefado no tomcat
+
+
+Para validar os serviços deverá ser utilizado uma ferramente restClient (Recomendo Advance Rest Client 'Plugin google chrome')
+
+Todas requisições são do tipo POST
+
+para Listar pontos de interesse cadastrados
+http://localhhost:8080/xyinc/POI/list
+
+Para listar pontos de interesse proximo a outro ponto, deverá ser passado os parametros (x,y,distancia)
+http://localhost:8080/xyinc/POI/listByProximity
+
+Para cadastrar um ponto de interesse, deverá ser passado os parametros (nome,x,y)
+http://localhost:8080/xyinc/POI/save
--- xyinc/database.sql
+++ xyinc/database.sql
@@ -20,7 +20,7 @@ CREATE OR REPLACE FUNCTION distancia_entre_pontos(
     lng2 numeric)
   RETURNS double precision AS
 $BODY$
-	SELECT 6371000 * acos(
+	SELECT 63.71 * acos(
 		sin( radians($1) ) * sin( radians( $3 ))
 	      + cos( radians($1) ) * cos( radians( $3 )) * cos(radians($4) - radians($2))  )
 	AS distance;
@@ -34,3 +34,12 @@ CREATE OR REPLACE FUNCTION fn_poi(
     IN distanciaIN int)
   RETURNS table(id integer, nome text, x numeric(26,20), y numeric(26,20), distancia double precision) AS
 'select * from (select id, nome, x, y, distancia_entre_pontos(y, x, yIN, xIN) distancia from poi where distancia_entre_pontos(y, x, yIN, xIN) < distanciaIN) T order by t.distancia' language 'sql';
+
+
+INSERT INTO POI (NOME,X,Y) VALUES ('Lanchonete', 27, 12);
+INSERT INTO POI (NOME,X,Y) VALUES ('Posto', 31, 18);
+INSERT INTO POI (NOME,X,Y) VALUES ('Joalheria', 15, 12);
+INSERT INTO POI (NOME,X,Y) VALUES ('Floricultura', 19, 21);
+INSERT INTO POI (NOME,X,Y) VALUES ('Pub', 12, 8);
+INSERT INTO POI (NOME,X,Y) VALUES ('Supermercado', 23, 6);
+INSERT INTO POI (NOME,X,Y) VALUES ('Churrascaria', 28, 2);
